7929c9f6bc17862f9cde4e7a26856294429cdcb5
Zavis <thezavis@gmail.com>
2025-02-26 03:15:05 +1100

    Entity - Tenant: User, Role, Permission, and mapping ManyToMany between Role and Permission

---
+++ src/main/java/com/aiswift/Tenant/Entity/Permission.java
@@ -0,0 +1,38 @@
+package com.aiswift.Tenant.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor	
+@AllArgsConstructor
+@Entity
+@Table(name="permissions")
+public class Permission {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column (nullable = false, length = 45)
+	private String name;
+	
+	@ManyToMany(mappedBy = "permissions")	
+	@JsonBackReference //mapping - hide roles, only show permission
+	private List<Role> roles = new ArrayList<>();
+		
+}
+
