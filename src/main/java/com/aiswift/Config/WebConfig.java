a32240bd1fb70df389c8c592951e074706bc8a7e
Zavis <thezavis@gmail.com>
2025-02-26 03:19:50 +1100

    Config: WebConfig to run Interceptor, Bcypt Config for PasswordEncoder

--- src/main/java/com/aiswift/Config/WebConfig.java
+++ src/main/java/com/aiswift/Config/WebConfig.java
@@ -1,5 +1,21 @@
 package com.aiswift.Config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebConfig {
+import com.aiswift.MultiTenancy.TenantInterceptor;
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	private final TenantInterceptor tenantInterceptor;
+	
+	public WebConfig(TenantInterceptor tenantInterceptor) {
+		this.tenantInterceptor = tenantInterceptor;
+	}
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(tenantInterceptor)
+		.addPathPatterns("/api/**");
+	}
+	
 
 }
