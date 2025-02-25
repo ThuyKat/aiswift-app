f410622bb6bbb6ccd8124e87b742300a3679caff
Zavis <thezavis@gmail.com>
2025-02-26 03:17:26 +1100

    Global classes: Service, Repository, and Controller, simple test case

---
+++ src/main/java/com/aiswift/Global/Controller/TenantController.java
@@ -0,0 +1,33 @@
+package com.aiswift.Global.Controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aiswift.Global.Service.TenantService;
+
+@RestController
+@RequestMapping("api/global")
+public class TenantController {
+	@Autowired
+	private TenantService tenantService;
+	
+	@PostMapping("/create-new-tenant")
+	public ResponseEntity<Object> createNewTenant(@RequestParam String shopName) {
+		try {
+			Long ownerId = (long) 1;
+			tenantService.createNewTenant(shopName, ownerId);
+			return new ResponseEntity<>(Map.of("message", "Successully add new Tenant"), HttpStatus.OK);
+			
+		}catch (RuntimeException e){
+			return new ResponseEntity<>(Map.of("error", e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
+		
+	}
+}
