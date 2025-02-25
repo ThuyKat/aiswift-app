bec79bfe52ddc322c9822b768c11253e604bc754
Zavis <thezavis@gmail.com>
2025-02-26 03:14:08 +1100

    Entity - Global: Developer, Owner, and Tenant

--- src/main/java/com/aiswift/Global/Entity/Developer.java
+++ src/main/java/com/aiswift/Global/Entity/Developer.java
@@ -1,5 +1,58 @@
 package com.aiswift.Global.Entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "developers", schema = "global_multi_tenant")
 public class Developer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name="first_name",nullable = false, unique = true, length = 45)
+	private String firstName;
+
+	@Column(name="last_name",nullable = false, unique = true, length = 45)
+	private String lastName;
+	
+	@Column(nullable = false, unique = true, length = 100)
+	private String email;
+
+	@Column(nullable = false, length = 255)
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	@Column
+	private Role role;
+
+	@Column(name = "created_at")
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@Column(name = "updated_at")
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	public enum Role {
+		SUPER_ADMIN, ADMIN
+	}
 }
