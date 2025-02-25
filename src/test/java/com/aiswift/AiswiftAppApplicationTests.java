d6fd144fb68d828b57f0921f03b52dc75f49f896
Zavis <thezavis@gmail.com>
2025-02-25 23:21:47 +1100

    Project: build project skeleton and add dependencise

---
+++ src/test/java/com/aiswift/AiswiftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiswiftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
