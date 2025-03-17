946c731df9f39644ac186164fad03c25d78a1e09
ThuyKat <katiefirst7@gmail.com>
2025-03-17 15:53:08 +1100

    deleted unused code

--- src/main/java/com/aiswift/dto/Global/CustomUserDetails.java
+++ src/main/java/com/aiswift/dto/Global/CustomUserDetails.java
@@ -13,31 +13,26 @@ import lombok.Data;
 
 @Data
 public class CustomUserDetails implements UserDetails {
-	
+
 	private static final long serialVersionUID = 1L;
 	private String email;
-    private String password;
-    private List<GrantedAuthority> authorities = new ArrayList<>();
-    private String dbName;
-    private Status status;
+	private String password;
+	private List<GrantedAuthority> authorities = new ArrayList<>();
+	private String dbName;
+	private Status status;
 
+	public CustomUserDetails(String email, String password, Status status, List<GrantedAuthority> authorities,
+			String dbName) {
+		this.email = email;
+		this.password = password;
+		this.status = status;
+		this.authorities = authorities;
+		this.dbName = dbName;
 
-    public CustomUserDetails(String email,String password,Status status,List<GrantedAuthority> authorities,String dbName) {
-    	this.email = email;
-    	this.password = password;
-    	this.status = status;
-    	this.authorities = authorities;
-    	this.dbName = dbName;
-    	
-    }
+	}
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-//		if(isGlobalUser) { 
-//			authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));//OWNER or DEVELOPER, if its owner add ADMIN  as ROLE
-//		}else {
-//	        authorities.add(new SimpleGrantedAuthority("ROLE_USER")); //ADMIN or STAFF, get from entity's role
-//
-//		}
 		return authorities;
 	}
 
@@ -52,24 +47,25 @@ public class CustomUserDetails implements UserDetails {
 		// TODO Auto-generated method stub
 		return email;
 	}
-	@Override
-    public boolean isAccountNonExpired() {
-        return true; // Or based on business logic
-    }
- 
- @Override
-    public boolean isAccountNonLocked() {
-        return status == Status.ACTIVE;
-    }
- @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-  @Override
-    public boolean isEnabled() {
-        return status == Status.ACTIVE;
-    }
- 
 
+	@Override
+	public boolean isAccountNonExpired() {
+		return true; // Or based on business logic
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return status == Status.ACTIVE;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return status == Status.ACTIVE;
+	}
 
 }
