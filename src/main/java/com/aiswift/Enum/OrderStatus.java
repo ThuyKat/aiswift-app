5ec839fb034697eac7ed6a964e710366400b933e
ThuyKat <katiefirst7@gmail.com>
2025-03-17 14:34:27 +1100

    added status for order processing

--- src/main/java/com/aiswift/Enum/OrderStatus.java
+++ src/main/java/com/aiswift/Enum/OrderStatus.java
@@ -7,5 +7,14 @@ import com.aiswift.Config.TenantDatabaseCondition;
 
 @Conditional(TenantDatabaseCondition.class)  // Only create for tenant databases
 public enum OrderStatus {
-	CREATED, COMPLETED,CANCELLED,REFUNDED
+	   CREATED,       // Initial order just created
+	    PENDING,       // Order awaiting payment confirmation
+	    PAID,          // Payment successful
+	    VOIDED, 		// Cancelled order (before processing)
+	    PROCESSING,    // Order being prepared
+	    SHIPPED,       // Order ready for pickup or delivery (optional)
+	    DELIVERED,     // Order received by customer
+	    
+	    REFUNDED	// Refunded order
+	    
 }
