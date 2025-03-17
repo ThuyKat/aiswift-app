a7c12fc1e2fd789b22678630af057a7162bd3e89
ThuyKat <katiefirst7@gmail.com>
2025-03-17 14:55:13 +1100

    added rest api for tenant controllers

---
+++ src/main/java/com/aiswift/Tenant/Controller/OrderController.java
@@ -0,0 +1,79 @@
+package com.aiswift.Tenant.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.aiswift.Config.TenantDatabaseCondition;
+import com.aiswift.Enum.OrderStatus;
+import com.aiswift.Tenant.Entity.Order;
+import com.aiswift.Tenant.Service.OrderDetailService;
+import com.aiswift.Tenant.Service.OrderService;
+import com.aiswift.dto.Tenant.OrderDetailDto;
+
+@Conditional(TenantDatabaseCondition.class) // Only create for tenant databases
+@RestController
+@RequestMapping("api/tenant/order")
+public class OrderController {
+	
+	@Autowired
+	OrderService orderService;
+	
+	@Autowired
+	OrderDetailService orderDetailService;
+	
+	@GetMapping("/")
+	public ResponseEntity<List<Order>> getAllOrders(){
+		List<Order>orders =  orderService.getAllOrders();
+		return ResponseEntity.ok(orders);
+		
+	}
+	
+	@GetMapping("/{orderId}")
+	public ResponseEntity<Order> getOrderById(@PathVariable Long orderId){
+		Order order =  orderService.getOrderById(orderId);
+		return ResponseEntity.ok(order);
+		
+	}
+	
+	@PutMapping("/{orderId}")
+	public ResponseEntity<Order>updateOrderStatus(@PathVariable Long orderId,@RequestParam(name="orderStatus",required=false) OrderStatus status){
+		Order order = orderService.getOrderById(orderId);
+		order.setStatus(status);
+		order = orderService.updateOrderStatusHistory(order,null);
+		return ResponseEntity.ok(order);
+	}
+	
+	@PutMapping("/{orderId}")
+	public ResponseEntity<Order>updateOrder(@PathVariable Long orderId, @RequestBody List<OrderDetailDto> orderDetailDtos){
+		Order order = orderService.getOrderById(orderId);
+		order = orderService.updateOrderWithItems(order, orderDetailDtos);
+		return ResponseEntity.ok(order);
+		
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<Order>createOrder(@RequestBody List<OrderDetailDto> orderDetailDtos){
+		Order order = orderService.createOrderWithItems(orderDetailDtos);
+		return ResponseEntity.ok(order);
+	}
+	
+	@DeleteMapping("/{orderId}")
+	public ResponseEntity<Order>cancelOrder(@PathVariable Long orderId){
+		Order order = orderService.getOrderById(orderId);
+		order = orderService.cancelOrder(order);
+		return ResponseEntity.ok(order);
+	}
+	
+
+}
---
+++ src/main/java/com/aiswift/Tenant/Controller/PaymentController.java
@@ -0,0 +1,83 @@
+package com.aiswift.Tenant.Controller;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aiswift.Config.TenantDatabaseCondition;
+import com.aiswift.Tenant.Entity.Order;
+import com.aiswift.Tenant.Entity.PayPal;
+import com.aiswift.Tenant.Service.OrderService;
+import com.aiswift.Tenant.Service.PaypalService;
+import com.aiswift.Tenant.Service.RefundService;
+import com.aiswift.dto.Tenant.PaypalPaymentResponse;
+import com.google.zxing.WriterException;
+import com.paypal.base.rest.PayPalRESTException;
+
+@Conditional(TenantDatabaseCondition.class) // Only create for tenant databases
+@RestController
+@RequestMapping("api/tenant/payment")
+public class PaymentController {
+	
+	@Autowired
+	PaypalService paypalService;
+	
+	@Autowired
+	OrderService orderService;
+	
+	@Autowired
+	RefundService refundService;
+
+	@GetMapping("/paypal/{orderId}")
+	public ResponseEntity<String> createPaypalPaymentWithQRCode(@RequestParam(name="email",required=true) String paypalEmail,@RequestParam(name="email",required=true)  BigDecimal totalPayment,@RequestParam(name="email",required=true)  String currency, @PathVariable Long orderId) throws WriterException, IOException, PayPalRESTException{
+		byte[] qrCodeBytes= paypalService.createPaymentWithQRCode(paypalEmail, totalPayment, currency, orderId);
+		String base64Image = Base64.getEncoder().encodeToString(qrCodeBytes);
+		return ResponseEntity.ok(base64Image);
+		
+	}
+	
+	@GetMapping("/paypal/confirm")
+	public ResponseEntity<Order> handlePaypalResponse(@RequestParam(name="token",required=true)String token, @RequestParam(name="paypal-email",required=true) String paypalEmail){
+		PayPal paypal = paypalService.findByPaypalEmail(paypalEmail);
+		PaypalPaymentResponse response = paypalService.captureOrderByToken(token, paypal);
+		String paymentStatus = response.getStatus();
+		// Safely extract order ID
+        if (response.getPurchaseUnits() == null || response.getPurchaseUnits().isEmpty()) {
+            throw new IllegalArgumentException("No purchase units found in PayPal response");
+        }
+		if("COMPLETED".equals(paymentStatus)) {
+			String paypalOrderId = response.getId();
+			String orderId = response.getPurchaseUnits().get(0).getReference_id();
+			Order order = orderService.getOrderById(Long.parseLong(orderId));
+			order = orderService.saveOrderPaidByPaypal(order,paypalOrderId);
+			return ResponseEntity.ok(order);
+			
+		}
+		   // Handle non-completed status
+		   throw new IllegalStateException("Payment not completed. Status: " + paymentStatus);
+	}
+	
+	@PostMapping("/cash/refund-request")
+	public ResponseEntity<Order>requestRefund(@RequestParam(name="orderId",required=true)Long orderId){
+		Order order = refundService.requestRefund(orderId);
+		return ResponseEntity.ok(order);
+	}
+	
+	@PostMapping("/cash/refund-approved-process")
+	public ResponseEntity<Order>processRefund(@RequestParam(name="orderId",required=true)Long orderId){
+		Order order = refundService.processRefund(orderId,true);
+		return ResponseEntity.ok(order);
+	}
+	
+	
+}
