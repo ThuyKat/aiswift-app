7e9f706b95694d9405c66c1be10a478b891eecd6
Zavis <thezavis@gmail.com>
2025-03-20 00:02:45 +1100

    GLOBAL ENTITY Developer, Owner, Tenant, updated code with additional fields

--- src/main/java/com/aiswift/Global/Entity/Developer.java
+++ src/main/java/com/aiswift/Global/Entity/Developer.java
@@ -1,58 +1,28 @@
 package com.aiswift.Global.Entity;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
+import com.aiswift.Common.Entity.BaseUser;
 import com.aiswift.Enum.Role;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode (callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "developers", schema = "global_multi_tenant")
-public class Developer {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	@Column(name="first_name",nullable = false, unique = true, length = 45)
-	private String firstName;
-
-	@Column(name="last_name",nullable = false, unique = true, length = 45)
-	private String lastName;
-	
-	@Column(nullable = false, unique = true, length = 100)
-	private String email;
-
-	@Column(nullable = false, length = 255)
-	private String password;
+public class Developer extends BaseUser{
 
 	@Enumerated(EnumType.STRING)
 	@Column
-	private Role role; //DEVELOPER
+	private Role role = Role.ADMIN;
 
-	@Column(name = "created_at")
-	@CreationTimestamp
-	private LocalDateTime createdAt;
-	
-	@Column(name = "updated_at")
-	@UpdateTimestamp
-	private LocalDateTime updatedAt;
-	
-	
 }
--- src/main/java/com/aiswift/Global/Entity/Owner.java
+++ src/main/java/com/aiswift/Global/Entity/Owner.java
@@ -2,30 +2,54 @@ package com.aiswift.Global.Entity;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.aiswift.Tenant.Entity.User;
+
+import com.aiswift.Common.Entity.BaseUser;
+import com.aiswift.Enum.Status;
+import com.aiswift.Tenant.Entity.Order;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
-@EqualsAndHashCode(callSuper = true) //This ensures that two TenantUser instances are considered equal only if their inherited (id, name, etc.) and specific (role) fields match.
+//@EqualsAndHashCode (callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "owners", schema = "global_multi_tenant")
-public class Owner extends User {
-
-	private static final long serialVersionUID = 1L;
+public class Owner extends BaseUser {
+	@Enumerated(EnumType.STRING)
+	@Column
+	private Status status = Status.ACTIVE;	
+	
 	@OneToMany (mappedBy = "owner", cascade = CascadeType.ALL) //owner lowecase, field in Tenant
 	@JsonIgnore
 	List<Tenant> tenants = new ArrayList<Tenant>();
 	
+	@Transient
+    private List<Order> orders = new ArrayList<>();
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "owner", fetch = FetchType.LAZY) //On delete restrict
+	private List<SubPlanDetail> subPlanDetails = new ArrayList<>();
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "owner", fetch = FetchType.LAZY) //On delete restrict
+	private List<Payment> payments = new ArrayList<>();
+	
+	@JsonIgnore
+	@OneToMany (mappedBy = "owner", fetch = FetchType.LAZY) 	
+	List<TenantActivityLog> tenantActivityLogs = new ArrayList<>();
 }
 
