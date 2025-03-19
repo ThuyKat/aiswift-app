45480489bfdea251d804e289208d17c601006508
Zavis <thezavis@gmail.com>
2025-03-20 00:15:11 +1100

    GLOBAL - REPOSITORY: Dev, Owner, Tenant change to Optional

--- src/main/java/com/aiswift/Global/Repository/TenantRepository.java
+++ src/main/java/com/aiswift/Global/Repository/TenantRepository.java
@@ -1,16 +1,18 @@
 package com.aiswift.Global.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.aiswift.Global.Entity.Tenant;
 
-
 @Repository
 public interface TenantRepository extends JpaRepository<Tenant, Long> {
-	Tenant findByName(String name);
+	Optional<Tenant> findByName(String name);
+
 	List<Tenant> findByOwnerId(Long ownerId);
-	Tenant findByDbName(String dbName);
+
+	Optional<Tenant> findByDbName(String dbName);
 }
