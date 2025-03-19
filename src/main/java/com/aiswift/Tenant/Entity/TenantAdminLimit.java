5ba18b3f665d3a608624265b1f25707e2702aaba
ThuyKat <katiefirst7@gmail.com>
2025-03-20 02:48:14 +1100

    deleted unused comments

--- src/main/java/com/aiswift/Tenant/Entity/TenantAdminLimit.java
+++ src/main/java/com/aiswift/Tenant/Entity/TenantAdminLimit.java
@@ -1,12 +1,8 @@
 package com.aiswift.Tenant.Entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
