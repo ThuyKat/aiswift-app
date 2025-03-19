530d9f5cb4746dc228ce2a6a81fa350c90b5f9fc
ThuyKat <katiefirst7@gmail.com>
2025-03-19 15:42:31 +1100

    fixed entity classes to specify relationship for jason back/manage reference, remove data annotation where it causes circular references

--- src/main/java/com/aiswift/Tenant/Entity/PayPal.java
+++ src/main/java/com/aiswift/Tenant/Entity/PayPal.java
@@ -3,7 +3,6 @@ import org.springframework.context.annotation.Conditional;
 import com.aiswift.Config.TenantDatabaseCondition;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,7 +15,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EntityListeners(OrderEntityListener.class)
 @Conditional(TenantDatabaseCondition.class) // Only create for tenant databases
 @Entity
 @Table(name = "paypal_configs")
