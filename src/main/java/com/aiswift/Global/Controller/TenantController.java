aa750f0e898f5d7a9edf5f697076c17d8f4e4c11
Zavis <thezavis@gmail.com>
2025-05-01 23:38:52 +1000

    Global - Tenant - Repository: findOwnerByEmail and Pagination

--- src/main/java/com/aiswift/Global/Controller/TenantController.java
+++ src/main/java/com/aiswift/Global/Controller/TenantController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,7 @@ import com.aiswift.Global.Service.TenantService;
 
 
 @RestController
+@Validated
 @RequestMapping("/api/owner")
 public class TenantController {
 	@Autowired
--- src/main/java/com/aiswift/Global/Repository/TenantRepository.java
+++ src/main/java/com/aiswift/Global/Repository/TenantRepository.java
@@ -3,9 +3,12 @@ package com.aiswift.Global.Repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.aiswift.Enum.Status;
 import com.aiswift.Global.Entity.Tenant;
 
 @Repository
@@ -13,6 +16,11 @@ public interface TenantRepository extends JpaRepository<Tenant, Long> {
 	Optional<Tenant> findByName(String name);
 
 	List<Tenant> findByOwnerId(Long ownerId);
-
+	
+	List<Tenant> findByOwnerEmail(String email);
+	
+	//SELECT * FROM tenant WHERE owner_id = ? AND status = ? ORDER BY created_at DESC LIMIT ? OFFSET ?;
+	Page<Tenant> findByOwnerIdAndStatusOrderByCreatedAtDesc(Pageable pageable, Long ownerId, Status status);
+	
 	Optional<Tenant> findByDbName(String dbName);
 }
